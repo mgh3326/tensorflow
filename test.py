99e1d5d6a1ecee08e5ea459e4946196bae188ccc
mgh3326 <mgh3326@naver.com>
2018-03-08 08:26:54 +0900

    update

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 
 sess = tf.Session()
+print("test")
 hello = tf.constant('Hello, TensorFlow')
 print(sess.run(hello))
-print(str(sess.run(hello), encoding='utf-8'))   # unicode --> utf-8
+print(str(sess.run(hello), encoding='utf-8'))  # unicode --> utf-8pytho
